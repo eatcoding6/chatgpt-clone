e038be186dada5f38d714a533f96339c2af729a6
Eatcoding <eatcoding@eatcoding.local>
2024-06-27 17:05:51 +0900

    SignUpForm markup

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,6 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Button>버튼</Button>
-    </main>
+    <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
   );
 }
