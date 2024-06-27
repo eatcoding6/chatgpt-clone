2b6364d7fad195945f966753813ab2c4fdaba94a
Eatcoding <eatcoding@eatcoding.local>
2024-06-27 19:15:48 +0900

    SignUpForm complete

--- components/auth/SignUpForm.tsx
+++ components/auth/SignUpForm.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { ChangeEvent } from "react";
+import { ChangeEvent, useEffect } from "react";
 import { Input } from "../ui/input";
 import { Label } from "../ui/label";
 import { FormCard } from "./FormCard";
@@ -8,23 +8,32 @@ import { useFormValidate } from "@/hooks/useFormValidate";
 import { SignUpSchema } from "@/schemas/auth";
 import { TSignUpFormError } from "@/types/form";
 import { FormMessage } from "./FormMessage";
+import { useFormState } from "react-dom";
+import { signUp } from "@/actions/signup";
+import toast from "react-hot-toast";
 
 export function SignUpForm() {
+  const [error, action] = useFormState(signUp, undefined);
   const { errors, validateField } =
     useFormValidate<TSignUpFormError>(SignUpSchema);
+
   const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
     const { name, value } = event.target;
     validateField(name, value);
   };
 
-  console.log("errors", errors);
+  useEffect(() => {
+    if (error?.errorMessage) {
+      toast.error(error.errorMessage);
+    }
+  }, [error]);
 
   return (
     <FormCard
       title="회원가입"
       footer={{ label: "이미 계정이 있으신가요?", href: "/login" }}
     >
-      <form className="space-y-6">
+      <form action={action} className="space-y-6">
         {/* 이름 */}
         <div className="space-y-1">
           <Label htmlFor="name">이름</Label>
